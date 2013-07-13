50b0c6c85ebde468eb94e2af5d5305bdf2837991
Ubuntu <ubuntu@ip-172-31-12-52.us-west-2.compute.internal>
2013-07-13 21:45:33 +0000

    Trying to do the file output

--- web.js
+++ web.js
@@ -1,3 +1,5 @@
+var fs = require('fs');
+
 var express = require('express');
 
 var app = express.createServer(express.logger());
@@ -10,3 +12,6 @@ var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
+var buffer = fs.readFileSync('index.html');
+response.send(buffer.toString());
